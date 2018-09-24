3c5d8fac74f02121fafab89c6dd9fc08e823ec63
Kamil Szczypior <kszczypior1@gmail.com>
2018-09-24 16:47:25 +0200

    03-jsx-function

--- src/index.js
+++ src/index.js
@@ -3,21 +3,27 @@ import ReactDOM from 'react-dom';
 import events from './data/events.json'; //dzięki webpackowi mozemy zaimportować tablicę w pliku json
 
 //LUB rozwiązanie z kursu
+const getEvents = () => {
+    return (
+        <ul>
+            {events.map(item => {
+                const date = new Date(item.date); //parsowanie stringa w tablicy na datę i przypisanie do zmiennej date.
+                if (date <= Date.now()) {
+                    return (
+                        <li key={item.id}>
+                            <strong>{item.name}</strong> <br/>
+                            Gdzie: {item.place}<br/>
+                            Kiedy: {item.date} - {item.time}
+                        </li>
+                    );
+                }
+                return null;
+            })}
+        </ul> 
+    )
+}
 
-ReactDOM.render(<ul>
-    {events.map(item => {
-        const date = new Date(item.date); //parsowanie stringa w tablicy na datę i przypisanie do zmiennej date.
-        if (date <= Date.now()) {
-            return (
-                <li key={item.id}>
-                    <strong>{item.name}</strong> <br/>
-                    Gdzie: {item.place}<br/>
-                    Kiedy: {item.date} - {item.time}
-                </li>
-            );
-        }
-    })}
-</ul>, document.getElementById('root'));
+ReactDOM.render(getEvents(), document.getElementById('root'));
 
 
 
